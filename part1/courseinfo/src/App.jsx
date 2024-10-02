c41a41f0300cedfc9c48b9fd653333041ee69ffc
Victor Rodríguez <vrodriguezbarbeira@gmail.com>
2024-10-02 21:40:35 +0200

    1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,54 +1,57 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
+  const course= 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name:  'State of a component',
+      exercises:  14
+    }
+  ]
+
+  const Header = (props)=>{
+    return <h1>{props.course}</h1>
   }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+
+  const Part=(props)=>{
+    return (
+          <p>
+            {props.name} {props.exercises}
+            </p>)
+    }
+  
+
+  const Content=(props)=>{
+    return (
+      <div>
+        {props.parts.map((part, index) => (
+          <Part key={index} name={part.name} exercises={part.exercises} />
+        ))}
+    </div>)
+   }
+  
+   const Total = (props) => {
+    const totalExercises = props.parts.reduce((sum, part) => sum + part.exercises, 0);
+    return <p>Total number of exercises {totalExercises}</p>;
+  };
 
 
   
-const Header = (props) => {
-console.log(props);
-return <h1>{props.course}</h1>
+  return (
+    <>
+    <Header course={course}/>
+    <Content parts={parts}/>
+    <Total parts={parts}/>
+    </>    
+      
+  )
   
-}
-
-const Part = (props)=>{
-  console.log(props)
-  return (
-    <p>{props.part}  {props.exercises} exercises</p>
-  )
-}
-const Content = () => {
-  return (
-    <div>
-      <Part part={part1.name}  exercises={part1.exercises}/>  
-      <Part part={part2.name}  exercises={part2.exercises}/>  
-      <Part part={part3.name}  exercises={part3.exercises}/>  
-    </div>
-  )
-}
-const Total = () => {
-  return (
-<p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
-  )
-}
-
-  return (
-    <div>
-      <Header course={course}/>
-      <Content />
-      <Total />
-    </div>
-  )
 }
 
 export default App
