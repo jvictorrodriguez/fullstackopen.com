7a91c0710a32169aa6832a1abe9135df06f38093
Victor Rodríguez <vrodriguezbarbeira@gmail.com>
2024-10-02 21:42:36 +0200

    1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,19 +1,37 @@
 const App = () => {
-  const course= 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name:  'State of a component',
-      exercises:  14
-    }
-  ]
+  // const course= 'Half Stack application development'
+  // const parts = [
+  //   {
+  //     name: 'Fundamentals of React',
+  //     exercises: 10
+  //   },
+  //   {
+  //     name: 'Using props to pass data',
+  //     exercises: 7
+  //   },
+  //   {
+  //     name:  'State of a component',
+  //     exercises:  14
+  //   }
+  // ]
+
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   const Header = (props)=>{
     return <h1>{props.course}</h1>
@@ -45,9 +63,9 @@ const App = () => {
   
   return (
     <>
-    <Header course={course}/>
-    <Content parts={parts}/>
-    <Total parts={parts}/>
+    <Header course={course.name}/>
+    <Content parts={course.parts}/>
+    <Total parts={course.parts}/>
     </>    
       
   )
