86ca14e1ee365a182c53158d0e8256edccd76347
Victor Rodríguez <vrodriguezbarbeira@gmail.com>
2024-10-04 17:25:31 +0200

    .

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,19 +1,4 @@
 const App = () => {
-  // const course= 'Half Stack application development'
-  // const parts = [
-  //   {
-  //     name: 'Fundamentals of React',
-  //     exercises: 10
-  //   },
-  //   {
-  //     name: 'Using props to pass data',
-  //     exercises: 7
-  //   },
-  //   {
-  //     name:  'State of a component',
-  //     exercises:  14
-  //   }
-  // ]
 
   const course = {
     name: 'Half Stack application development',
